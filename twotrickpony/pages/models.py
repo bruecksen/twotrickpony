41a5d014a16dd7d2a1ee3a2a1a7b8c0013ea947a
Matthias Brück <hi@brueck.io>
2023-02-07 13:48:19 +0100

    Add email to sender

--- twotrickpony/pages/models.py
+++ twotrickpony/pages/models.py
@@ -9,6 +9,7 @@ from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.fields import StreamField, RichTextField
 from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
+from wagtail.admin.mail import send_mail
 from wagtail.images.blocks import ImageChooserBlock
 
 from twotrickpony.pages.blocks import AccordionBlock, AccordionElement
@@ -26,6 +27,7 @@ class HomePage(AbstractEmailForm):
         ('faq', AccordionBlock(child_block=AccordionElement()))
     ], use_json_field=True)
     thank_you_text = RichTextField(blank=True)
+    thank_you_email_text = models.TextField(blank=True)
     content_panels = AbstractEmailForm.content_panels + [
         MultiFieldPanel([
             FieldPanel('event_date'),
@@ -34,6 +36,7 @@ class HomePage(AbstractEmailForm):
         FieldPanel('body'),
         InlinePanel('form_fields', label="Form fields"),
         FieldPanel('thank_you_text'),
+        FieldPanel('thank_you_email_text'),
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('from_address', classname="col6"),
@@ -42,3 +45,20 @@ class HomePage(AbstractEmailForm):
             FieldPanel('subject'),
         ], "Email"),
     ]
+
+    def send_mail(self, form):
+        super().send_mail(form)
+        self.send_thank_you_email(form)
+
+
+    # send a mail to applicant, boolean has to be set in the form
+    def send_thank_you_email(self, form):
+        address = []
+
+        # try to find user e-mail and parse content
+        for field in form:
+            if (field.label.lower() == 'e-mail') or (field.label.lower() == 'email') or (field.label.lower() == 'e mail'):
+                address.append(field.value())
+        if address:
+            content = "{}\n\n-----\n{}".format(self.thank_you_email_text, self.render_email(form))
+            send_mail(self.subject, content, [self.to_address], address[0])
